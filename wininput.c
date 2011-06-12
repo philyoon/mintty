philyoon/mintty

commit 464ecbb520d2fc6b9a3f43559a8e54bd9c638aa0

Don't override Ctrl+symbol combinations that yield control characters when only at modifyOtherKeys level 1. Issue 268.
git-svn-id: http://mintty.googlecode.com/svn/trunk@1193 2782a51e-c42b-11dd-927d-974589228060

wininput.c
@@ -700,19 +700,17 @@ win_key_down(WPARAM wp, LPARAM lp)
         ?: layout()
         ?: app_pad_code(key - VK_NUMPAD0 + '0');
     when 'A' ... 'Z' or ' ':
-      if ((key != ' ' && alt_code_key(key - 'A' + 0xA)) || char_key())
-        break;
-      if (term.modify_other_keys > 1)
-        modify_other_key();
-      else if (!ctrl_key())
-        ctrl_ch(CTRL(key));
+      key != ' ' && alt_code_key(key - 'A' + 0xA) ?:
+      char_key() ?:
+      term.modify_other_keys <= 1 ? ctrl_key() :
+      term.modify_other_keys ? modify_other_key() :
+      ctrl_ch(CTRL(key));
     when '0' ... '9' or VK_OEM_1 ... VK_OEM_102:
-      if ((key <= '9' && alt_code_key(key - '0')) || char_key())
-        break;
-      if (term.modify_other_keys)
-        modify_other_key();
-      else if (!ctrl_key())
-        app_pad_code(key <= '9' ? key : key - VK_OEM_PLUS + '+');
+      key <= '9' && alt_code_key(key - '0') ?:
+      char_key() ?:
+      term.modify_other_keys <= 1 && ctrl_key() ?:
+      term.modify_other_keys ? modify_other_key() :
+      app_pad_code(key <= '9' ? key : key - VK_OEM_PLUS + '+');
     otherwise:
       return 0;
   }

commit 0e7bcf45b55195c36d6d8799a642c647760183f3

Restore Ctrl+letter control character support on non-Latin keyboards, broken in r1193.

wininput.c
@@ -705,7 +705,7 @@ win_key_down(WPARAM wp, LPARAM lp)
     when 'A' ... 'Z' or ' ':
       key != ' ' && alt_code_key(key - 'A' + 0xA) ?:
       char_key() ?:
-      term.modify_other_keys <= 1 ? ctrl_key() :
+      term.modify_other_keys <= 1 && ctrl_key() ?:
       term.modify_other_keys ? modify_other_key() :
       ctrl_ch(CTRL(key));
     when '0' ... '9' or VK_OEM_1 ... VK_OEM_102:
@@ -716,7 +716,6 @@ win_key_down(WPARAM wp, LPARAM lp)
       key <= '9' ? app_pad_code(key) :
       VK_OEM_PLUS <= key && key <= VK_OEM_PERIOD
       ? app_pad_code(key - VK_OEM_PLUS + '+') : 0;
-      ;
     otherwise:
       return 0;
   }